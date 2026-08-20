4cd9667a841811d2adea9a4fd38dbfec64d04a02
soundaryamanivannan08-source <soundaryamanivannan08@gmail.com>
2026-08-20 13:03:10 +0530

    Delete fix_hero_margin.py

--- fix_hero_margin.py
+++
@@ -1,7 +0,0 @@
-import os
-
-css_file = 'styles.css'
-with open(css_file, 'a', encoding='utf-8') as f:
-    f.write('\n/* Fix for fixed navbar overlapping hero sections */\n.navbar + * {\n    margin-top: 90px;\n}\n')
-
-print("Added margin-top to hero sections in styles.css")
